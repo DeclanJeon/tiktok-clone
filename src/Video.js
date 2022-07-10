57cc1e1d7f7abb0a750134c0bc8beea499edc15c
declan <syas0301@gmail.com>
2022-07-10 17:10:36 +0900

    tiktok-clone first commit

---
+++ src/Video.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Video.css";
+
+function Video() {
+    return (
+        <div className='video'>
+            <h1>This is Video</h1>
+            {/* <Video Footer */}
+            {/* <Video Sidebar */}
+        </div>
+    );
+}
+
+export default Video;
