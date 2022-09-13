5765a33b88c3763564b35424b9ac13d978940f0e
Declan <36153766+DeclanJeon@users.noreply.github.com>
2022-09-13 14:15:24 +0900

    Update db.js

--- db.js
+++ db.js
@@ -1,9 +1,3 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBLspYk3pTXPtCT_cSMFTuwbetXvJ4KNJ4",
-    authDomain: "tiktok-clone-3c2e2.firebaseapp.com",
-    projectId: "tiktok-clone-3c2e2",
-    storageBucket: "tiktok-clone-3c2e2.appspot.com",
-    messagingSenderId: "516577591229",
-    appId: "1:516577591229:web:4a574b3334426647551cd4",
-    measurementId: "G-GB3T7BC3ZJ",
+    
 };
